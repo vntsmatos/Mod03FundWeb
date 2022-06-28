e523f380d31cb78a7681ddb053b9130b25cf5d85
vntsmatos <ovinidev@gmail.com>
2022-06-28 10:03:50 -0300

    @vntsmatos[Task:Criacao Array e Dicionario]{10:03}

---
+++ AulaArrayJs/js/main.js
@@ -0,0 +1,32 @@
+/* start Lista de Dicionário
+_________________________________________*/
+var frutas =[{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/* start Dicionário
+__________________________________________
+
+var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+End Dicionário
+__________________________________________*/
+
+
+
+/* start Array
+__________________________________________*/
+
+//var lista = ["maca", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.ttotring());
+//console.log(lista.join("    "));
+
+/* End Array 
+__________________________________________*/
