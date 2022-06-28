b6c864a3e01e372ffbf7bb61bbfc38c1caac3937
vntsmatos <ovinidev@gmail.com>
2022-06-28 10:44:35 -0300

    @vntsmatos[Task:Condicionais|Lacos de repeticao|Date]{28/06/2022|10:44}

---
+++ AulaCondicionais/js/main.js
@@ -0,0 +1,65 @@
+/* start Date
+_______________________________________
+
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+End Date
+______________________________________*/
+
+
+
+
+
+
+/* start estrutura for
+________________________________________
+
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+
+End estrutura for 
+_______________________________________*/
+
+
+
+
+
+
+/* start Laço de Repetição(While)
+__________________________________________
+
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+
+End Laço de Repetição
+__________________________________________*/
+
+
+
+
+
+/* start Condicional
+___________________________________________
+
+
+var idade = prompt("Qual sua idade: ");
+var idade =18;
+if (idade >=18){
+    alert("maior de idade");
+}else{
+    alert("manor de idade");
+
+};
+
+End Condicional
+__________________________________________*/
