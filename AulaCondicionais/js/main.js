0f67104b72d5844bc80883b6b9b85d15023b9f04
vntsmatos <ovinidev@gmail.com>
2022-06-28 11:06:12 -0300

    @vntsmatos[Task: Trabalhando Funcoes]{28/06/2022|11:06}

--- AulaCondicionais/js/main.js
+++ AulaCondicionais/js/main.js
@@ -1,3 +1,39 @@
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade =prompt("Qual sua idade");
+console.log(validaIdade(idade));
+
+/* start Função Soma
+______________________________________
+
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+End Função Soma
+______________________________________*/
+
+
 /* start Date
 _______________________________________
 
