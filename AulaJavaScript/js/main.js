7cf7fbe4cafa4299e039f15b59a6bf57b7cc8f64
vntsmatos <ovinidev@gmail.com>
2022-06-28 09:48:08 -0300

    @vntsmatos[Task:Intro JS]{28/06/2022|09:48}

---
+++ AulaJavaScript/js/main.js
@@ -0,0 +1,10 @@
+
+var nome = "Vinicius Santos";
+var n1 = 5;
+var n2 = 3;
+
+var frase = "Japão é o melhor time do mundo";
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
